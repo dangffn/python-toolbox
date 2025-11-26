30125dcc1802f4c091b9ef2480567c33964e08c6
Dan Griffin <dangffn@gmail.com>
2025-11-26 06:48:32 -0700

    added SSH file browser TUI via textual

--- toolbox/subcommands/net.py
+++ toolbox/subcommands/net.py
@@ -6,6 +6,7 @@ from rich.table import Table, Column
 from toolbox.subcommands.loader import register
 from toolbox.logger import console
 from toolbox.net.ipv4 import Config
+from toolbox.net.ssh_browser import ssh_browser
 
 
 def show_ip(ip_address: str, output: Literal["print", "json"]) -> None:
@@ -37,3 +38,10 @@ def setup_net_ipv4(parser: argparse.ArgumentParser) -> None:
     parser.add_argument("ip_address", help="IPv4 address in CIDR notation ie. 0.0.0.0/0")
     parser.add_argument("--output", default="print", choices=["print", "json"], help="Output format")
     parser.set_defaults(func=show_ip)
+    
+    
+@register("net", "ssh-browser", description="Browse files on a remote system over SSH in your CLI")
+def setup_net_ssh_browser(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument("connection_str", help="SSH resource to browse (ie. <user>@<hostname>:<path>). If <user> is omitted, the current account will be used. If <path> is omitted, will default to root")
+    parser.add_argument("--port", "-p", default=22, type=int, help="The SSH port to use, default: 22.")
+    parser.set_defaults(func=ssh_browser)
